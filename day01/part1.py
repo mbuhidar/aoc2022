3f12c6890c2cbaf551a957de2b4f2dae738048b5
Michael Buhidar <mbuhidar@gmail.com>
2022-12-01 06:07:49 -0500

    day1, part1 solution

---
+++ day01/part1.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    print(s)
+    lines = s.splitlines()
+
+    elf_sum = 0
+    high_sum = 0
+
+    for line in lines:
+        if line == '':
+            if elf_sum > high_sum:
+                high_sum = elf_sum
+                print(high_sum)
+            elf_sum = 0
+            continue
+
+        elf_sum += int(line)
+    return high_sum 
+
+
+INPUT_S = '''\
+
+'''
+EXPECTED = 1
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        ('1\r1\r\r2\r4', 6),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
