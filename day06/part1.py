d8db16717cea4da0d272922d3ddc46b7582c7f0f
Michael Buhidar <mbuhidar@gmail.com>
2022-12-06 23:53:22 -0500

    day6, part1

---
+++ day06/part1.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    for count, _ in enumerate(s):
+        phrase = s[:4]
+        if len(set(phrase)) == 4:
+            return count + 4
+        s = s[1:]
+    return 0
+
+
+INPUT_S = '''\
+nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+'''
+EXPECTED = 10
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
